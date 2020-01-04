f553b5576bab8c53e5915640986f8ec51157c63a
Mohammad Mehrnia <qermezkon@gmail.com>
2020-01-04 22:03:56 +0330

    Add project files.

---
+++ SendMessageToDiafaan/frmMain.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace SendMessageToDiafaan
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.lblWordCount = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblResultInfo = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblWordCount,
+            this.lblResultInfo});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 192);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(569, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnSend.Location = new System.Drawing.Point(0, 160);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(569, 32);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "ارسال به سرور Diafaan";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtMessage.Location = new System.Drawing.Point(0, 0);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(569, 160);
+            this.txtMessage.TabIndex = 7;
+            this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
+            // 
+            // lblWordCount
+            // 
+            this.lblWordCount.Name = "lblWordCount";
+            this.lblWordCount.Size = new System.Drawing.Size(70, 17);
+            this.lblWordCount.Text = "تعداد کاراکتر:";
+            // 
+            // lblResultInfo
+            // 
+            this.lblResultInfo.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultInfo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblResultInfo.Name = "lblResultInfo";
+            this.lblResultInfo.Size = new System.Drawing.Size(0, 17);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 214);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.statusStrip1);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ارسال پیام";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblWordCount;
+        private System.Windows.Forms.ToolStripStatusLabel lblResultInfo;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtMessage;
+    }
+}
+
---
+++ SendMessageToDiafaan/frmMain.cs
@@ -0,0 +1,103 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SendMessageToDiafaan
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtMessage.Text.Trim()))
+            {
+                await sendHttpsMessage(txtMessage.Text);
+            }
+            else
+            {
+                lblResultInfo.Text = "متنی جهت ارسال وارد نشده است";
+            }
+        }
+
+        private async Task sendHttpsMessage(string message)
+        {
+            btnSend.Enabled = false;
+            var cancellationTokenSource = new CancellationTokenSource();
+            var client = new RestClient("https://Mehrnia-MSI:9710/http/send-message");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddParameter("to", "+989163085306");
+            request.AddParameter("message-type", "sms.automatic");
+            request.AddParameter("message", message);
+            request.AddParameter("username", "admin");
+            request.AddParameter("password", "kabinet95");
+
+            ServicePointManager.ServerCertificateValidationCallback = new
+            RemoteCertificateValidationCallback
+            (
+               delegate { return true; }
+            );
+            IRestResponse response = await client.ExecuteTaskAsync(request, cancellationTokenSource.Token);
+
+            if (response.IsSuccessful)
+            {
+                var context = response.Content.Split(':')[1];
+                lblResultInfo.Text=$"پیام با شناسه {context} ارسال شد";
+                btnSend.Enabled = true;
+            }
+            else
+            {
+                lblResultInfo.Text = "پیام ارسال نشد";
+                btnSend.Enabled = true;
+            }
+        }
+
+        private async Task sendHttpMessage(string message)
+        {
+            btnSend.Enabled = false;
+            var cancellationTokenSource = new CancellationTokenSource();
+            var client = new RestClient("httpd://Mehrnia-MSI:9710/http/send-message");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddParameter("to", "+989163085306");
+            request.AddParameter("message-type", "sms.automatic");
+            request.AddParameter("message", message);
+            request.AddParameter("username", "admin");
+            request.AddParameter("password", "kabinet95");
+
+            IRestResponse response = await client.ExecuteTaskAsync(request, cancellationTokenSource.Token);
+
+            if (response.IsSuccessful)
+            {
+                var context = response.Content.Split(':')[1];
+                lblResultInfo.Text = $"پیام با شناسه {context} ارسال شد";
+                btnSend.Enabled = true;
+            }
+            else
+            {
+                lblResultInfo.Text = "پیام ارسال نشد";
+                btnSend.Enabled = true;
+            }
+        }
+        private void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+            lblWordCount.Text = $"تعداد کاراکتر:{txtMessage.Text.Length + 1}";
+        }
+    }
+}
